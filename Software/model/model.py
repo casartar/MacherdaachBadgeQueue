71e035c4d7df9bac3b7967e6649b2a19f0b004e9
T-Hellmund <tobias.hellmund@iosb.fraunhofer.de>
2021-09-04 23:34:15 +0200

    refactor to model-view-controller patern

---
+++ Software/model/model.py
@@ -0,0 +1,19 @@
+from Software.model import place
+
+
+class Model(object):
+
+    def __init__(self, numberOfPlaces):
+        self.numberOfPlaces = numberOfPlaces;
+        self.list_of_labels_to_display_ticket_number = [];
+        self.list_of_labels_to_display_place_number = [];
+        self.list_of_labels_to_display_in_queue = [];
+        # list_of_ticket_numbers is only used if ticket number could not be registered to a place
+        self.list_of_ticket_numbers = []
+        self.list_of_processing_times = []
+        self.list_of_places = []
+        self.initPlaces();
+
+    def initPlaces(self):  # Initialize list_of_places
+        for i in range(self.numberOfPlaces):
+            self.list_of_places.append(place.Place())
