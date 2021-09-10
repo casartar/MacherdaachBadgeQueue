b5c19a971901c90cd221b09e0cfc0491e35fbbd9
JohnnyMoonlight <hellmund.tobias@web.de>
2021-09-10 23:32:00 +0200

    set up initial tests for controller

--- Software/controller/test_controller.py
+++ Software/controller/test_controller.py
@@ -4,30 +4,24 @@ from unittest import TestCase
 from controller import Controller
 from model.place import Place, PlaceState
 
+c = Controller()
 
 class TestController(TestCase):
+
     def test_reserve_place_for_ticket_number(self):
-        c = Controller()
         reserved_place = c.reservePlaceForTicketNumber(1, 1)
         self.assertTrue(reserved_place == Place(PlaceState.REGISTERED, 1))
 
-
-class TestController(TestCase):
     def test_occupy_place(self):
         c = Controller()
         occupied_place = c.occupyPlace(1)
         self.assertTrue(occupied_place.state == PlaceState.OCCUPIED)
-
-
-class TestController(TestCase):
 
     def test_handle_message_from_place_for_illegal_json(self):
         illegal_json_from_place = json.loads('{"place_number": 1,"place_occupiedTYPOERROR": false}')
         c = Controller()
         self.assertRaises(Exception, c.handle_message_from_place, illegal_json_from_place)
 
-
-class TestController(TestCase):
     def test_handle_new_ticket_number(self):
         new_ticket_json = json.loads('{"new_number":1}')
         c = Controller()
